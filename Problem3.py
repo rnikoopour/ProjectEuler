99a1a238f9896a5ae01bfe1d2bafb45bd7354f4d
deadheartsbeat <keepitreza2@gmail.com>
2014-07-04 10:50:57 -0700

    Finished Problem 3

---
+++ Problem3.py
@@ -0,0 +1,13 @@
+def main():
+    num = 600851475143
+    i = 2
+    
+    while i * i < num:
+        while (num % i) == 0:
+            num = num / i
+        i += 1
+    
+    print num
+            
+if __name__ == '__main__':
+    main()
